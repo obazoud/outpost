7b958f5f063fe1fe7907adec943b69179505defd
Phil Leggetter <phil@leggetter.co.uk>
2025-01-20 14:14:29 +0000

    refactor(docs): move links to LINKS single definition

---
+++ website/src/links.ts
@@ -0,0 +1,14 @@
+const GITHUB = "https://github.com/hookdeck/outpost";
+
+export const LINKS = {
+  github: GITHUB,
+  docs: `${GITHUB}/tree/main/docs`,
+  docs_quickstarts: `${GITHUB}/tree/main/docs/1-get-started`,
+  docs_overview: `${GITHUB}/tree/main/docs/2-overview`,
+  docs_features: `${GITHUB}/tree/main/docs/3-features`,
+  docs_guides: `${GITHUB}/tree/main/docs/4-guides`,
+  docs_references: `${GITHUB}/tree/main/docs/5-references`,
+  issues: `${GITHUB}/issues`,
+  releases: `${GITHUB}/releases`,
+  contribute: `${GITHUB}/blob/main/CONTRIBUTING.md`,
+}
